ac8abd5500ec0e75901de6c544ec5b40892d5ab8
= <danishfaisal.wwe@gmail.com>
2022-04-16 12:50:15 +0530

    Layout styling & react-router-dom added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <div className="app">
+      </div>
+    </>
   );
 }
 
