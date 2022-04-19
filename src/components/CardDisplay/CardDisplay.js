a9b4458030b90f17d6606d9f4f56fa08d8a99133
= <danishfaisal.wwe@gmail.com>
2022-04-19 22:46:37 +0530

    Modal comp created

--- src/components/CardDisplay/CardDisplay.js
+++ src/components/CardDisplay/CardDisplay.js
@@ -1,11 +1,12 @@
 import { Badge } from '@material-ui/core';
 import React from 'react';
 import { img_300, unavailable } from '../../config/config';
+import MoreInfoModal from '../MoreInfoModal/MoreInfoModal';
 import "./CardDisplay.css";
 
 const CardDisplay = ({ id, date, title, poster, media_type, vote_average }) => {
     return (
-        <div className="media">
+        <MoreInfoModal media_type={media_type} id={id}>
             <Badge overlap="rectangular" badgeContent={vote_average} color={vote_average > 6 ? 'primary' : 'secondary'} />
             <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
             <b className="title">{title}</b>
@@ -13,7 +14,7 @@ const CardDisplay = ({ id, date, title, poster, media_type, vote_average }) => {
                 {media_type === "tv" ? "TV Series" : "Movie"}
                 <span className="subTitle">{date}</span>
             </span>
-        </div>
+        </MoreInfoModal>
     )
 }
 
---
+++ src/components/MoreInfoModal/MoreInfoModal.js
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import axios from 'axios';
+import { img_500, unavailable, unavailableLandscape } from '../../config/config';
+import "./MoreInfoModal.css";
+import { Button } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    paper: {
+        width: "90%",
+        height: "80%",
+        backgroundColor: "#39445a",
+        border: "1px solid #282c34",
+        borderRadius: 10,
+        color: "white",
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(1, 1, 3)
+    },
+}));
+
+export default function MoreInfoModal({ media_type, id, children }) {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [reqData, setReqData] = useState({});
+    const [video, setVideo] = useState();
+
+    const fetchClicked = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
+
+        setReqData(data);
+    }
+
+    const fetchVideo = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
+
+        setVideo(data.results[0]?.key);
+    }
+
+    useEffect(() => {
+        fetchClicked();
+        fetchVideo();
+        // eslint-disable-next-line
+    }, []);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <div
+                className="media"
+                style={{ cursor: "pointer" }}
+                color="inherit"
+                onClick={handleOpen}
+            >
+                {children}
+            </div>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    {reqData && (
+                        <div className={classes.paper}>
+                            <div className="ContentModal">
+                                <img
+                                    src={
+                                        reqData.poster_path
+                                            ? `${img_500}/${reqData.poster_path}`
+                                            : unavailable
+                                    }
+                                    alt={reqData.name || reqData.title}
+                                    className="ContentModal__portrait"
+                                />
+                                <img
+                                    src={
+                                        reqData.backdrop_path
+                                            ? `${img_500}/${reqData.backdrop_path}`
+                                            : unavailableLandscape
+                                    }
+                                    alt={reqData.name || reqData.title}
+                                    className="ContentModal__landscape"
+                                />
+                                <div className="ContentModal__about">
+                                    <span className="ContentModal__title">
+                                        {reqData.name || reqData.title} (
+                                        {(
+                                            reqData.first_air_date ||
+                                            reqData.release_date ||
+                                            "-----"
+                                        ).substring(0, 4)}
+                                        )
+                                    </span>
+                                    {reqData.tagline && (
+                                        <i className="tagline">{reqData.tagline}</i>
+                                    )}
+
+                                    <span className="ContentModal__description">
+                                        {reqData.overview}
+                                    </span>
+
+                                    <Button
+                                        variant="contained"
+                                        startIcon={<YouTubeIcon />}
+                                        color="secondary"
+                                        target="__blank"
+                                        href={`https://www.youtube.com/watch?v=${video}`}
+                                    >
+                                        Watch the Trailer
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </Fade>
+            </Modal>
+        </>
+    );
+}
